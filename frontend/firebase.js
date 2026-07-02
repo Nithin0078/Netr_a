9f1f853169d9cacf1cfc2d5da8c594a015b06beb
Nithin Chandra <knithinchandra2004@gmail.com>
2026-07-02 10:01:58 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8Pjle2OQ91CTe7pIPiWkJc-fzDp0b8vI",
-    authDomain: "netra-3.firebaseapp.com",
-    projectId: "netra-3",
-    storageBucket: "netra-3.firebasestorage.app",
-    messagingSenderId: "724023660492",
-    appId: "1:724023660492:web:aaa48e473d67313dadd769"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
